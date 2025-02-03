5340ed018231c5528975931319508bf86156021f
jegomez26 <gomez.jenel26@gmail.com>
2025-02-03 18:39:42 +0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+      e.preventDefault();
+      document.querySelector(this.getAttribute('href')).scrollIntoView({
+        behavior: 'smooth'
+      });
+    });
+  });
+
+  // Initialize AOS
+AOS.init({
+  duration: 1000, // Animation duration
+  once: true, // Animation only happens once
+});
